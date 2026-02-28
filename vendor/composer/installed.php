4d17a3ac8d3be738dfc9f52f9aff0600ba3b640d
kokkkr <kokooooer@gmail.com>
2026-02-28 14:18:14 +0200

    upload vendor/composer/installed.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,78 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'cyborx/secure-login',
+  ),
+  'versions' => 
+  array (
+    'cyborx/secure-login' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'graham-campbell/result-type' => 
+    array (
+      'pretty_version' => 'v1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3ba905c11371512af9d9bdd27d99b782216b6945',
+    ),
+    'phpoption/phpoption' => 
+    array (
+      'pretty_version' => '1.9.3',
+      'version' => '1.9.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e3fac8b24f56113f7cb96af14958c0dd16330f54',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.32.0',
+      'version' => '1.32.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a3cc8b044a6ea513310cbd48ef7333b384945638',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.32.0',
+      'version' => '1.32.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6d857f4d76bd4b343eac26d6b539585d2bc56493',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.32.0',
+      'version' => '1.32.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0cc9dd0f17f61d8131e7df6b84bd344899fe2608',
+    ),
+    'vlucas/phpdotenv' => 
+    array (
+      'pretty_version' => 'v5.6.2',
+      'version' => '5.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '24ac4c74f91ee2c193fa1aaa5c249cb0822809af',
+    ),
+  ),
+);
