f1eb26fe34f8ae0bbc8fd17cd85f48fc2305c730
kokkkr <kokooooer@gmail.com>
2026-02-28 17:13:49 +0200

    Update redirect path to dashboard in telegram_auth.php

--- telegram_auth.php
+++ telegram_auth.php
@@ -88,7 +88,7 @@ try {
         Telegram::sendMessage($botToken, $announceChat, $text, 'HTML');
     }
 
-    $next = '/app/dashboard';
+    $next = '/index.php?page=dashboard';
 
     if (!empty($_GET['state']) &&
         is_string($_GET['state']) &&
